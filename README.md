7f5b7ec59fa522f3ee678294237f3c47bb48936e
ambersanti <carolinasandoval879@gmail.com>
2024-01-01 15:00:14 -0600

    Changed license type of prompt to list and added 3 different licenses and their output. Also added link to walktrough video to the README file.

--- README.md
+++ README.md
@@ -1,5 +1,5 @@
 
-PROJECT TITLE: Hola Mundo
+PROJECT TITLE: MEDITRACK
 
 TABLE OF CONTENT
 -[LICENSE](#LICENSE)
@@ -11,24 +11,26 @@ TABLE OF CONTENT
 -[CONTACT INFORMATION](#CONTACT-INFORMATION)
 
 # LICENSE
-  MIT
+  GNU GENERAL PUBLIC LICENSE (2007, Free Software Foundation). The GNU General Public License is a free, copyleft license for software and other kinds of works. The licenses for most software and other practical works are designed to take away your freedom to share and change the works.  By contrast, the GNU General Public License is intended to guarantee your freedom to share and change all versions of a program--to make sure it remains free software for all its users.  We, the Free Software Foundation, use the GNU General Public License for most of our software; it applies also toany other work released this way by its authors.  You can apply it to your programs, too.
 
 # DESCRIPTION
-  Es un proyecto que saluda al mundo
+  Command-line application that generates a README file using the user inputs.
 
 # INSTALLATION INFORMATION
-  npm i
+  Install inquirer@8.2.4 (npm i inquirer@8.2.4)
 
 # USAGE INFORMATION
-  Ingresa tus citass y tus tratamientos
+  Enter npm install and fill the blank spaces with the information that is being asked in the command-line regarding the README file. 
 
 # CONTRIBUTION GUIDELINES
-  ADDOK
+  Carolina Sandoval - creator and owner of this README-generator.
 
 # TESTS
-  DSFA
+  No test instructions
 
 # CONTACT INFORMATION
 For further questions reach me on GitHub and by email.
-GITHUB: https://github.com/ADFA
-EMAIL: ASF 
+GITHUB: https://github.com/ambersanti1
+EMAIL: carolinasandoval879@gmail.com 
+
+Link of the walkthrough video: https://youtu.be/9T4cvmMvN1A
--- index.js
+++ index.js
@@ -70,9 +70,10 @@ inquirer
       message: 'Enter usage information:'
     },
     {
-      type: 'input',
+      type: 'list',
       name: 'license',
       message: 'Choose a type of license (MIT, GNU, BSD):',
+      choices: ['MIT', 'GNU', 'BSD'],
     },
     {
       type: 'input',
@@ -96,8 +97,16 @@ inquirer
     },
   ])
   .then((answers) => {
-    const readmePageContent = generateREADME(answers);
+    if (answers.license === "MIT") {
+      answers.license = 'MIT License (2023, Ryan Messett). Permission is hereby granted, free of charge, to any person obtaining a copy of this software and associated documentation files (the "Software"), to deal in the Software without restriction, including without limitation the rights to use, copy, modify, merge, publish, distribute, sublicense, and/or sell copies of the Software, and to permit persons to whom the Software is furnished to do so, subject to the following conditions: The above copyright notice and this permission notice shall be included in all copies or substantial portions of the Software.';
 
+    } else if (answers.license === "GNU") {
+      answers.license = 'GNU GENERAL PUBLIC LICENSE (2007, Free Software Foundation). The GNU General Public License is a free, copyleft license for software and other kinds of works. The licenses for most software and other practical works are designed to take away your freedom to share and change the works.  By contrast, the GNU General Public License is intended to guarantee your freedom to share and change all versions of a program--to make sure it remains free software for all its users.  We, the Free Software Foundation, use the GNU General Public License for most of our software; it applies also toany other work released this way by its authors.  You can apply it to your programs, too.';
+
+    } else if (answers.license === "BSD") {
+      answers.license = 'BSD 2-Clause License (2024, ambersanti1). Redistribution and use in source and binary forms, with or without modification, are permitted provided that the following conditions are met: 1. Redistributions of source code must retain the above copyright notice, this list of conditions and the following disclaimer. 2. Redistributions in binary form must reproduce the above copyright notice, this list of conditions and the following disclaimer in the documentation and/or other materials provided with the distribution.';
+    }
+    const readmePageContent = generateREADME(answers);
     fs.writeFile('README.md', readmePageContent, (err) =>
       err ? console.log(err) : console.log('Successfully created README.md!')
     );
